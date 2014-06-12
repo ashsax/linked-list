716d8f7b5226c5c6960bec31d8a85f82ebe68025
ashsax <ashwin.222@gmail.com>
2014-06-11 19:13:16 -0700

    separated test driver from class, finished all basic functions

---
+++ LinkedList.php
@@ -0,0 +1,102 @@
+<?php
+	class Node {
+		public $data;
+		public $next;
+
+		public function __construct($value) {
+			$this->data = $value;
+			$this->next = null;
+		}
+	}
+
+	class LinkedList {
+		private $head;
+		private $tail;
+
+		public function __construct() {
+			$head = null;
+			$tail = null;
+		}
+
+		public function printList() {
+			$node = $this->head;
+			echo "{ ";
+			while ($node != null) {
+				echo $node->data;
+				$node = $node->next;
+				if ($node != null) {
+					echo ",";
+				}
+				echo " ";
+			}
+			echo "} <br/>";
+		}
+
+		public function insertFront($data) {
+			$node = new Node($data);
+			// if empty list
+			if ($this->head == null && $this->tail == null) {
+				$this->head = $node;
+				$this->tail = $node;
+			}
+			else {
+				// $oldHead = $this->head;
+				// $this->head = $node;
+				// $this->head->next = $oldHead;
+				$node->next = $this->head;
+				$this->head = $node;
+			}
+		}
+
+		public function insertBack($data) {
+			$node = new Node($data);
+			// empty list: same as insertFront()
+			if ($this->head == null && $this->tail == null) {
+				insertFront($data);
+			}
+			else {
+				$this->tail->next = $node;
+				$this->tail = $node;
+			}
+		}
+
+		public function removeFront() {
+			$node = $this->head;
+			if ($node == null) {
+				throw new Exception("ERROR: Can't remove elements from an empty list.");
+			}
+			// only 1 element
+			if ($this->head == $this->tail && $this->head != null) {
+				$this->head = null;
+				$this->tail = null;
+			}
+			else if ($node != null) {
+				$this->head = $this->head->next;
+			}
+			return $node->data;
+		}
+		public function removeBack() {
+			$node = $this->tail;
+			if ($node == null) {
+				throw new Exception("ERROR: Can't remove elements from an empty list.");
+			}
+			// only 1 element
+			if ($this->head == $this->tail && $this->head != null) {
+				$this->head = null;
+				$this->tail = null;
+			}
+			else if ($node != null) {
+				// traversing the whole list to find
+				// the second-to-last node...
+				// the pain of making a singly-linked list
+				$newTail = $this->head;
+				while ($newTail->next != $this->tail) {
+					$newTail = $newTail->next;
+				}
+				$newTail->next = null;
+				$this->tail = $newTail;
+			}
+			return $node->data;
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,75 +1,7 @@
 <?php
-	class Node {
-		public $data;
-		public $next;
+	include("LinkedList.php");
 
-		public function __construct($value) {
-			$this->data = $value;
-			$this->next = null;
-		}
-	}
-
-	class LinkedList {
-		private $head;
-		private $tail;
-
-		public function __construct() {
-			$head = null;
-			$tail = null;
-		}
-
-		public function printList() {
-			$node = $this->head;
-			echo "{ ";
-			while ($node != null) {
-				echo $node->data;
-				$node = $node->next;
-				if ($node != null) {
-					echo ",";
-				}
-				echo " ";
-			}
-			echo "} <br/>";
-		}
-
-		public function insertFront($data) {
-			$node = new Node($data);
-			// if empty list
-			if ($this->head == null && $this->tail == null) {
-				$this->head = $node;
-				$this->tail = $node;
-			}
-			else {
-				// $oldHead = $this->head;
-				// $this->head = $node;
-				// $this->head->next = $oldHead;
-				$node->next = $this->head;
-				$this->head = $node;
-			}
-		}
-
-		public function insertBack($data) {
-			$node = new Node($data);
-			// empty list: same as insertFront()
-			if ($this->head == null && $this->tail == null) {
-				insertFront($data);
-			}
-			else {
-				$this->tail->next = $node;
-				$this->tail = $node;
-			}
-		}
-
-		public function removeFront() {
-			$node = $this->head;
-			if ($node != null) {
-				$this->head = $this->head->next;
-			}
-			return $node->data;
-		}
-	}
-
-	function main() {
+	try {
 		$list = new LinkedList();
 		$list->insertFront(5);
 		$list->insertFront(2);
@@ -77,9 +9,20 @@
 		$list->insertFront(3);
 		$list->insertBack(7);
 		$list->printList();
-		echo "Removed " . $list->removeFront() . " from list. <br/>";
+		echo "Removed " . $list->removeFront() . " from front of list. <br/>";
+		$list->printList();
+		echo "Removed " . $list->removeBack() . " from back of list. <br/>";
+		$list->printList();
+		echo "Removed " . $list->removeFront() . " from front of list. <br/>";
+		$list->printList();
+		echo "Removed " . $list->removeBack() . " from back of list. <br/>";
+		$list->printList();
+		echo "Removed " . $list->removeFront() . " from front of list. <br/>";
+		$list->printList();
+		echo "Removed " . $list->removeBack() . " from back of list. <br/>";
 		$list->printList();
 	}
-
-	main();
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
 ?>
